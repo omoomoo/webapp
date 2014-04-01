26b184edd9a7644733ce7e4e005f0bcdf382b7b1
cokepluscarbon <93948886@qq.com>
2014-04-02 00:55:32 +0800

    fileupload init

---
+++ src/main/java/com/demo/webapp/web/controller/FileUploadController.java
@@ -0,0 +1,31 @@
+package com.demo.webapp.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping(value = "/fileupload")
+public class FileUploadController {
+	private static final Logger logger = LoggerFactory.getLogger(FileUploadController.class);
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String getFileUploadPage() {
+		return "/fileupload/upload";
+	}
+
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	public String upload(@RequestParam("file") MultipartFile file) {
+		logger.info("{}", file.getContentType());
+		logger.info("{}", file.getName());
+		logger.info("{}", file.getOriginalFilename());
+		logger.info("{}", file.getSize());
+
+		return "/fileupload/upload";
+	}
+
+}
