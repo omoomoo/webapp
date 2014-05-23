922e4d7959990571ffc250cd51951ec054556475
cokepluscarbon <93948886@qq.com>
2014-05-23 09:34:25 +0800

    Update security_user.sql

--- src/main/resources/security_user.sql
+++ src/main/resources/security_user.sql
@@ -1,4 +1,4 @@
-INSERT INTO `security_user` (`id`, `email`, `enabled`, `password`, `username`) VALUES (1, 'loveorz@vip.qq.com', true, '123456', 'tiger@allwinnertech.com');
-INSERT INTO `security_user` (`id`, `email`, `enabled`, `password`, `username`) VALUES (2, 'email', false, 'password', 'cokepluscarbon');
-INSERT INTO `security_user` (`id`, `email`, `enabled`, `password`, `username`) VALUES (3, 'test', false, 'test', 'test');
-INSERT INTO `security_user` (`id`, `email`, `enabled`, `password`, `username`) VALUES (4, 'pig@qq.com', true, 'pigpassword', 'pig');
+INSERT INTO `security_user` (`id`, `email`, `enabled`, `password`, `username`) VALUES (1, 'loveorz@vip.qq.com', true, 'passwd', 'tiger@allwinnertech.com');
+INSERT INTO `security_user` (`id`, `email`, `enabled`, `password`, `username`) VALUES (2, 'email', false, 'passwd', 'cokepluscarbon');
+INSERT INTO `security_user` (`id`, `email`, `enabled`, `password`, `username`) VALUES (3, 'test', false, 'paswd', 'test');
+INSERT INTO `security_user` (`id`, `email`, `enabled`, `password`, `username`) VALUES (4, 'pig@qq.com', true, 'passwd', 'pig');
